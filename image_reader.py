f273026303fe27ab4ee3a7b1429d7d663b91e52c
Octavian <omarian@lampix.co>
2018-05-19 17:15:33 +0300

    saved train images local

--- image_reader.py
+++ image_reader.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as et
 import os
+import cv2
+import numpy as np
 
 path_to_xml = r'E:\Licenta\Licenta\Resources\Annotations'
 path_to_images = r'E:\Licenta\Licenta\Resources\pasc_data\validation'
@@ -27,4 +29,24 @@ def get_unique_labels():
     print(len(labels))
 
 
-get_unique_labels()
+def get_cifar_data(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def plot_images_to_folder():
+    image_number = 10000
+    for data_batch in range(2, 6):
+        dict = get_cifar_data(r'E:\Licenta\Licenta\Resources\cifar\cifar-10-batches-py\data_batch_' + str(data_batch))
+        for image in dict[b'data']:
+            im_r = im_r = image[0:1024].reshape(32, 32)
+            im_g = image[1024:2048].reshape(32, 32)
+            im_b = image[2048:].reshape(32, 32)
+            img = np.dstack((im_b, im_g, im_r))
+            cv2.imwrite(r'./TrainImages/' + str(image_number) + '.jpg', img)
+            image_number += 1
+
+
+plot_images_to_folder()
